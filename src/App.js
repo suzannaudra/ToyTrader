1b77ab70ef362b00d06d4d7bbc4ae20dca7c71eb
Suzann Kowalski <suzannaudra@gmail.com>
2020-06-13 11:39:22 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Navbar from "./components/navbar.component";
+import ToyList from "./components/ToyList.component";
+import EditToy from "./components/EditToy.component";
+import CreateToy from "./components/CreateToy.component";
+import CreateUser from "./components/CreateUser.component";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+      <Navbar />
+      <br/>
+      <Route path="/" exact component={ToyList} />
+      <Route path="/edit/:id" component={EditToy} />
+      <Route path="/create" component={CreateToy} />
+      <Route path="/user" component={CreateUser} />
+      </div>
+    </Router>
+    
   );
 }
 
