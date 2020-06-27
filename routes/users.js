38d92b1f63b4857ad6fef8b5f04e97218c7f6c5e
Angel de la Torre <angel.dlt.1996@gmail.com>
2020-06-26 19:23:40 -0700

    adding changes from last night that weren't pushed apparently

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 let User = require("../models/users");
+const bcrypt = require("bcrypt");
 
 // Will get all the toys the user has listed
 router.route("/user").get((req, res) => {
@@ -10,21 +11,26 @@ router.route("/user").get((req, res) => {
 });
 
 // Will add a new user
-router.route("/user/add").post((req, res) => {
+router.route("/user/add").post(async (req, res) => {
   const firstName = req.body.firstName;
   const lastName = req.body.lastName;
   const email = req.body.email;
   const password = req.body.password;
 
-  // Creating new User Model
-  const newUser = new User({ firstName, lastName, email, password });
+  try {
+    // User bcrypt to get a hashed password for security purposes
+    const hashedPassword = await bcrypt.hash(password, 10);
+    //Add the new user to the user database
+    // User the current date to create a unique ID
+    // Creating new User Model
+    const newUser = new User({ firstName, lastName, email, hashedPassword });
 
-  console.log(newUser);
-  // Saves the new user only if the email is unique/not a duplicate
-  newUser
-    .save()
-    .then(() => res.json("User added!"))
-    .catch(err => res.status(400).json("Error: " + err));
+    console.log(newUser);
+    // Saves the new user only if the email is unique/not a duplicate
+    newUser.save().then(() => res.json("User added!"));
+  } catch (err) {
+    res.status(400).json("Error: " + err);
+  }
 });
 
 //Replace id with actual userid
