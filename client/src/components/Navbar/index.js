076de6125db2a29561f36c95950e2bc9aa934a32
Angel de la Torre <angel.dlt.1996@gmail.com>
2020-07-18 17:28:04 -0700

    Slowly changing hrefs to Link Components

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
-import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from "react-bootstrap";
+import {
+  Navbar,
+  Nav,
+  NavLink,
+  NavDropdown,
+  Form,
+  FormControl,
+  Button
+} from "react-bootstrap";
 import "./style.css";
-
 
 export default class NavigationBar extends Component {
   render() {
@@ -11,19 +18,38 @@ export default class NavigationBar extends Component {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="/toys">HOME</Nav.Link>
-            <Nav.Link href="/aboutUs">ABOUT US</Nav.Link>
+            <NavLink to="/toys">HOME</NavLink>
+            <NavLink to="/aboutUs">ABOUT US</NavLink>
             <NavDropdown title="CONTACT" id="basic-nav-dropdown">
-              <NavDropdown.Item href="https://github.com/thuynguyen-nht">Thuy Nguyen</NavDropdown.Item>
-              <NavDropdown.Item href="https://github.com/ardealto">Angel de la Torre</NavDropdown.Item>
-              <NavDropdown.Item href="https://github.com/suzannaudra">Suzann Kowalski</NavDropdown.Item>
-              <NavDropdown.Item href="https://github.com/Bangdrum">Ryan Shepard</NavDropdown.Item>
+              <NavDropdown.Item href="https://github.com/thuynguyen-nht">
+                Thuy Nguyen
+              </NavDropdown.Item>
+              <NavDropdown.Item href="https://github.com/ardealto">
+                Angel de la Torre
+              </NavDropdown.Item>
+              <NavDropdown.Item href="https://github.com/suzannaudra">
+                Suzann Kowalski
+              </NavDropdown.Item>
+              <NavDropdown.Item href="https://github.com/Bangdrum">
+                Ryan Shepard
+              </NavDropdown.Item>
             </NavDropdown>
           </Nav>
           <Form inline>
-            <FormControl type="text" placeholder="Find a toy!" className="Searchbar text-center" value={this.props.query}
-              onChange={this.props.onChange}/>
-            <Button variant="outline-info" onClick={this.props.clickHandler} onKeyPress={this.props.onKeyPress}>Search</Button>
+            <FormControl
+              type="text"
+              placeholder="Find a toy!"
+              className="Searchbar text-center"
+              value={this.props.query}
+              onChange={this.props.onChange}
+            />
+            <Button
+              variant="outline-info"
+              onClick={this.props.clickHandler}
+              onKeyPress={this.props.onKeyPress}
+            >
+              Search
+            </Button>
           </Form>
         </Navbar.Collapse>
       </Navbar>
